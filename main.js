e5e21bd594deb43477171a1b60a207b4c6c16eb6
armand <45756314+urbain4@users.noreply.github.com>
2019-01-04 15:06:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-messagepublicite="ça te dis du l'argent comme si il  en pleuvait?";
+messagepublicite="ça te dis de l'argent comme si il  en pleuvait?";
 
 function alertepub() {
     if (confirm(messagepublicite)) {
-    txt = "Ok";
-    window.location.assign("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-  } else {
-    txt = "Cancel";
+    
+        txt = "Ok";
+        window.location.assign("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  } 
+    else {
+        txt = "Cancel";
   }
 }
 
